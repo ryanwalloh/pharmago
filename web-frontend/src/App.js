16efe23d85c07fee26b5e05e7a10fec3404a8357
Ryan Walloh <ryanwalloh@outlook.com>
2025-09-04 01:07:53 +0800

    Added new page PharmacyRegistration2 (UNFINISHED)

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { RegistrationProvider } from './contexts/RegistrationContext';
 import LandingPage from './components/LandingPage';
 import PharmacyRegistration from './components/PharmacyRegistration';
+import PharmacyRegistration2 from './components/PharmacyRegistration2';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/pharmacy-registration" element={<PharmacyRegistration />} />
+          <Route path="/pharmacy-registration-2" element={<PharmacyRegistration2 />} />
         </Routes>
       </Router>
     </RegistrationProvider>
