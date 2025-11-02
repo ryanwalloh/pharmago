ef7fa8b35d205ee0a10a5bb14fff989b0f5a1678
Ryan Walloh <ryanwalloh@outlook.com>
2025-11-02 17:20:22 +0800

    preview build for customer-app 9 fixing app crash

--- mobileapp/apps/customer-app/utils/fonts.ts
+++ mobileapp/apps/customer-app/utils/fonts.ts
@@ -1,12 +1,5 @@
-import * as Font from 'expo-font';
-
-export const loadFonts = async () => {
-  await Font.loadAsync({
-    'Nexa-ExtraLight': require('../assets/fonts/Nexa-ExtraLight.ttf'),
-    'Nexa-Heavy': require('../assets/fonts/Nexa-Heavy.ttf'),
-  });
-};
-
+// Font family constants
+// Fonts are loaded in app/_layout.tsx using useFonts hook
 export const fontFamily = {
   light: 'Nexa-ExtraLight',
   heavy: 'Nexa-Heavy',
